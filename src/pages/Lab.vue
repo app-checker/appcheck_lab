ec08867358f636754dbb3e01c4a2bbe0d4a29d2e
龙馨竹 <chenhonzhou@gmail.com>
2022-06-27 21:47:04 +0800

    fix: hot reload `正在解析文件`
    v-show 总是会显示(css 的 display: none/block), 所以在热加载之后不论 `isParsing` 的值与否, 还是会显示

--- src/pages/Lab.vue
+++ src/pages/Lab.vue
@@ -69,7 +69,7 @@
 
     </div>
 
-    <div v-show="isParsing" style="display: flex;align-items: center;justify-content: center; margin-top: 24px">
+    <div v-if="isParsing" style="display: flex;align-items: center;justify-content: center; margin-top: 24px">
       <div class="vue-loading" style="fill: #333;width: 50px;height: 50px;">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
           <path d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
